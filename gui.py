eb69292519ea27928c7b2ae61646b85242b9a6a2
Geyrgan <jamalhasan1465@gmail.com>
2025-06-01 14:45:05 +0500

    Imported libraries

---
+++ gui.py
@@ -0,0 +1,6 @@
+import os
+import sys
+import tkinter as tk
+from tkinter import ttk, filedialog,scrolledtext,messagebox
+from PIL import ImageTk, Image
+from pygments.styles.dracula import background
